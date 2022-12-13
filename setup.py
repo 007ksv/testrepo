abf038bf6e53722e0a24f435c218f10b9d78fbff
Keshav Sain <46397837+007ksv@users.noreply.github.com>
2022-12-14 00:41:37 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+# with open("requirements.txt") as f:
+# 	install_requires = f.read().strip().split("\n")
+
+# get version from __version__ variable in proshanti_lms/__init__.py
+# from proshanti_lms import __version__ as version
+
+setup(
+	name="lms",
+	version="1.1",
+	description="LMS",
+	author="TVF",
+	author_email="tvft@gmail.com",
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=install_requires
+)
