882a60c6f94b2499d7b2f0e5e7104b250f07a15a
Keshav Sain <ksv@gmail.com>
2022-12-13 19:54:35 +0530

    update version

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in proshanti_lms/__init__.py
-# from proshanti_lms import __version__ as version
+from lms import __version__ as version
 
 setup(
 	name="lms",
-	version="1.1",
+	version=version,
 	description="LMS",
 	author="TVF",
 	author_email="tvft@gmail.com",
